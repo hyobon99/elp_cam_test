6d2cc643b5476344880a4ced3faa761d829de2ab
HyoBonKoo <93653078+hyobon99@users.noreply.github.com>
2025-08-19 20:03:27 +0900

    Update sdk_definitions.h

--- test_linux_sdk/sdk_deps/OSD-Linux_H264_AP_0724/sdk_definitions.h
+++ test_linux_sdk/sdk_deps/OSD-Linux_H264_AP_0724/sdk_definitions.h
@@ -4,15 +4,6 @@
 // 누락된 전역 변수 정의
 extern int Dbg_Param;
 
-// H.264 포맷 구조체 정의
-struct H264Format {
-    unsigned short wWidth;
-    unsigned short wHeight;
-    unsigned int FrameSize;
-    unsigned short fpsCnt;
-    unsigned int FrPay[32];  // Frame Payload 배열
-};
-
 // 전역 H.264 포맷 변수
 extern struct H264Format *gH264fmt;
 
